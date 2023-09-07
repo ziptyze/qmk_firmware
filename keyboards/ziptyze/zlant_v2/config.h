649d47bc38e1f34f4a3396f1d88d3e63bead3008
ziptyze <mwcordier@gmail.com>
2023-09-07 10:21:00 -0400

    add zlant v2

---
+++ keyboards/ziptyze/zlant_v2/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 ziptyze
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
